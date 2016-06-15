4b067c141b559a50467a8ca8efcb5d14c76b6573
lypan <xxzero12@gmail.com>
2016-06-15 17:28:38 +0800

    Day9

--- 283. Move Zeroes.cpp
+++ 283. Move Zeroes.cpp
@@ -1,14 +1,31 @@
+// // first zero will stop
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int cur = 0;
+//         int first_zero = 0;
+//         while(cur < nums.size()){
+//             if(nums[cur] != 0){
+//                 swap(nums[cur], nums[first_zero]);
+//                 first_zero ++;
+//             }
+//             cur ++;
+//         }
+//     }
+// };
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int cur = 0;
-        int non_zero = 0;
-        while(cur < nums.size()){
-            if(nums[cur] != 0){
-                swap(nums[cur], nums[non_zero]);
-                non_zero ++;
+        int idx = 0;
+        for(auto&& n : nums) {
+            if(n != 0) {
+                nums[idx ++] = n;
             }
-            cur ++;
         }
+        for(int i = idx; i < nums.size(); i ++) {
+            nums[i] = 0;
+        }
+        
     }
 };
